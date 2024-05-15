889c91ea9e496f1cb17b479a888d8e1e59d35924
paedayx <phasit@skilllane.com>
2024-05-15 18:07:15 +0700

    feat: remove unuse import

--- app/chains/conversation_session_chain.py
+++ app/chains/conversation_session_chain.py
@@ -1,14 +1,10 @@
-import bs4
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
